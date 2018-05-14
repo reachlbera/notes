6a06881066223ae2ae6f0989d05fefe80b8e46ba
wqf <wqf@tenmind.com>
2018-05-14 18:00:41 +0800

    tvt

--- app/assets/javascripts/plugins/editor/ueditor.all.js
+++ app/assets/javascripts/plugins/editor/ueditor.all.js
@@ -11591,79 +11591,6 @@ UE.plugins['undo'] = function () {
     }
 };
 
-
-// plugins/copy.js
-UE.plugin.register('copy', function () {
-
-    var me = this;
-
-    function initZeroClipboard() {
-
-        ZeroClipboard.config({
-            debug: false,
-            swfPath: me.options.UEDITOR_HOME_URL + 'third-party/zeroclipboard/ZeroClipboard.swf'
-        });
-
-        var client = me.zeroclipboard = new ZeroClipboard();
-
-        // 复制内容
-        client.on('copy', function (e) {
-            var client = e.client,
-                rng = me.selection.getRange(),
-                div = document.createElement('div');
-
-            div.appendChild(rng.cloneContents());
-            client.setText(div.innerText || div.textContent);
-            client.setHtml(div.innerHTML);
-            rng.select();
-        });
-        // hover事件传递到target
-        client.on('mouseover mouseout', function (e) {
-            var target = e.target;
-            if (e.type == 'mouseover') {
-                domUtils.addClass(target, 'edui-state-hover');
-            } else if (e.type == 'mouseout') {
-                domUtils.removeClasses(target, 'edui-state-hover');
-            }
-        });
-        // flash加载不成功
-        client.on('wrongflash noflash', function () {
-            ZeroClipboard.destroy();
-        });
-    }
-
-    return {
-        bindEvents: {
-            'ready': function () {
-                if (!browser.ie) {
-                    if (window.ZeroClipboard) {
-                        initZeroClipboard();
-                    } else {
-                        utils.loadFile(document, {
-                            src: me.options.UEDITOR_HOME_URL + "third-party/zeroclipboard/ZeroClipboard.js",
-                            tag: "script",
-                            type: "text/javascript",
-                            defer: "defer"
-                        }, function () {
-                            initZeroClipboard();
-                        });
-                    }
-                }
-            }
-        },
-        commands: {
-            'copy': {
-                execCommand: function (cmd) {
-                    if (!me.document.execCommand('copy')) {
-                        alert(me.getLang('copymsg'));
-                    }
-                }
-            }
-        }
-    }
-});
-
-
 // plugins/paste.js
 ///import core
 ///import plugins/inserthtml.js
@@ -18934,19 +18861,6 @@ UE.plugins['tablesort'] = function () {
     };
 };
 
-
-// plugins/contextmenu.js
-///import core
-///commands 右键菜单
-///commandsName  ContextMenu
-///commandsTitle  右键菜单
-/**
- * 右键菜单
- * @function
- * @name baidu.editor.plugins.contextmenu
- * @author zhanyi
- */
-
 UE.plugins['contextmenu'] = function () {
     var me = this;
     me.setOpt('enableContextMenu',true);
@@ -19415,18 +19329,6 @@ UE.plugins['contextmenu'] = function () {
             if ( ieRange.item ) {
                 var range = new dom.Range( me.document );
                 range.selectNode( ieRange.item( 0 ) ).select( true, true );
-            }
-        }
-    });
-
-    // 添加复制的flash按钮
-    me.addListener('aftershowcontextmenu', function(type, menu) {
-        if (me.zeroclipboard) {
-            var items = menu.items;
-            for (var key in items) {
-                if (items[key].className == 'edui-for-copy') {
-                    me.zeroclipboard.clip(items[key].getDom());
-                }
             }
         }
     });
